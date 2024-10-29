9ffc80a3561ad024743b73a2812645d96cf4dfe4
silas <workitoutsilas@gmail.com>
2024-10-29 16:32:46 +0000

    Services section on homepage

--- myvaltec/src/App.js
+++ myvaltec/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import MainSlider from './pages/home/components/carousel.jsx'
 import AboutSection from './pages/home/components/aboutSection.jsx';
+import Example from './pages/home/services.jsx';
 
 function App() {
   return (
     <>
         <MainSlider/>
         <AboutSection/>
+        <Example/>
     </>
-
   );
 }
 
