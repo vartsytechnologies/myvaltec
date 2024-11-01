1e3915e96df993871e205ba9c8bb473325f156ff
vartsytechnologies <vartsytech@gmail.com>
2024-11-01 18:07:31 +0000

    working on the files for the temporary hosting. put the react router in the app js and changed the app js to the home js

--- myvaltec/src/home.jsx
+++ myvaltec/src/home.jsx
@@ -1,24 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './home' // Adjust paths as necessary
-import Thinking from './pages/Thinking/src/Thinking'; // Adjust paths as necessary
-
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={Thinking} />
-        {/* Add more routes as needed */}
-      </Switch>
-    </Router>
-  );
-}
-
-export default App;
-
-
-/*import React from "react";
+import React from "react";
 import MainSlider from "./pages/home/components/carousel.jsx";
 import AboutSection from "./pages/home/components/aboutSection.jsx";
 
@@ -30,7 +10,7 @@ import NavBar from "./components/navbar/navbar.jsx";
 import ContactSection from "./pages/home/contactSection.jsx";
 import Footer from "./components/Footer/Footer.js";
 
-function App() {
+function Home() {
   return (
     <>
       <NavBar />
@@ -47,5 +27,4 @@ function App() {
   );
 }
 
-export default App;
-*/
+export default Home;
