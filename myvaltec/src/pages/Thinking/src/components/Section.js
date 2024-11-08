676b9edfd454078f984fee506484c2d39aa9840c
silas <workitoutsilas@gmail.com>
2024-11-08 10:23:41 +0000

    Updates seen

--- myvaltec/src/pages/Thinking/src/components/Section.js
+++ myvaltec/src/pages/Thinking/src/components/Section.js
@@ -1,29 +1,35 @@
-
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import CompanyOverview from '../../../../assets/companyOverview.jpg';
-import { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Import the CSS styles
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CompanyOverview from "../../../../assets/companyOverview.jpg";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import the CSS styles
 
 function Section({ title, content, extraContent }) {
   useEffect(() => {
     AOS.init({
       duration: 950, // Animation duration in milliseconds
-      offset: 50,     // Trigger animations 50px before an element is in view
+      offset: 50, // Trigger animations 50px before an element is in view
     });
   }, []);
   return (
     <Container className="my-5">
-      <h2 className="text-danger" data-aos="fade-down">{title}</h2>
+      <h2 className="text-danger" data-aos="fade-down">
+        {title}
+      </h2>
       <Row>
         <Col md={8}>
           <p data-aos="zoom-in-right">{content}</p>
         </Col>
         <Col md={4}>
-          <img src={CompanyOverview} alt={title} className="img-fluid" data-aos="fade-up"/>
+          <img
+            src={CompanyOverview}
+            alt={title}
+            className="img-fluid"
+            data-aos="fade-up"
+          />
         </Col>
       </Row>
       <p>{extraContent}</p>
@@ -32,4 +38,3 @@ function Section({ title, content, extraContent }) {
 }
 
 export default Section;
-
