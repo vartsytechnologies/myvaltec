ab913e64b9ee1742e9f0bcf1fce9f1133a6f6ca1
caVeDevv <workitoutsilas@gmail.com>
2025-02-08 18:28:14 +0000

    services pages, needs styling and images

--- src/pages/services/Extras.jsx
+++ src/pages/services/Extras.jsx
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
 import Banner from "../../components/banner/Banner";
+import { useEffect } from "react";
 import { Container, Row, Col, Modal, Button, Carousel } from "react-bootstrap";
-import { serviceItems } from "./actualServices.jsx"; // Import serviceItems
+import { serviceItems } from "./actualServices.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const ServiceDetails = () => {
   const { slug } = useParams();
   const service = serviceItems.find((s) => s.slug === slug);
 
+  useEffect(() => {
+    AOS.init({
+      duration: 900,
+      offset: 200,
+    });
+  }, []);
+
+  window.scrollTo(0, 0);
   if (!service) {
     return <h2>Service not found</h2>;
   }
@@ -19,9 +30,13 @@ const ServiceDetails = () => {
           <div>
             {service.name !== "Sustainability" &&
               service.name !== "Digital Solutions" && (
-                <p className="defbodyFont">{service.description}</p>
+                <p className="defbodyFont" data-aos="fade-up">
+                  {service.description}
+                </p>
               )}
-            <div className="defbodyFont">{service.detailedContent}</div>
+            <div className="defbodyFont" data-aos="fade-down">
+              {service.detailedContent}
+            </div>
           </div>
         </Container>
       </Container>
