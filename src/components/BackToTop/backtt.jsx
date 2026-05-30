c5fe16bac477e0ea70a27765486f6f4f8c3a432b
caVeDevv <workitoutsilas@gmail.com>
2026-05-30 07:57:35 -0700

    updates

--- src/components/BackToTop/backtt.jsx
+++ src/components/BackToTop/backtt.jsx
@@ -1,23 +1,15 @@
-import { useState, useEffect } from "react";
-// import { motion } from "framer-motion";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-// import { FaArrowUp } from "react-icons/fa";
 
 const BackToTop = () => {
-  const [scrollY, setScrollY] = useState(0);
   const { pathname } = useLocation();
   const body = document.getElementsByTagName("body")[0];
 
   useEffect(() => {
     body.scrollTo({ top: 0, left: 0, behavior: "auto" });
-  }, [pathname]);
-
-  useEffect(() => {
-    const handleScroll = () => setScrollY(body.scrollTop > 300);
-    body.addEventListener("scroll", handleScroll);
-    return () => body.removeEventListener("scroll", handleScroll);
-  }, []);
+  }, [pathname, body]);
 
   return null;
 };
+
 export default BackToTop;
